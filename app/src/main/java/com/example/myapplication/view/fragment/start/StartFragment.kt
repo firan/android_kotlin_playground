ed7ba21b6521a675956a067e8f3aa0f9a60b9f12
Artur <arnie@vp.pl>
2019-02-25 23:02:40 +0100

    Navigation

--- app/src/main/java/com/example/myapplication/view/fragment/start/StartFragment.kt
+++ app/src/main/java/com/example/myapplication/view/fragment/start/StartFragment.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.view.fragment.start
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,16 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import com.example.myapplication.R
-import com.example.myapplication.usecase.account.Logout
-import com.example.myapplication.view.activity.loginactivity.LoginActivity
 import kotlinx.android.synthetic.main.fragment_start.*
-import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class StartFragment : Fragment() {
 
     private val startViewModel: StartViewModel by viewModel()
-    private val logout: Logout by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,15 +29,5 @@ class StartFragment : Fragment() {
             if(!users.isNullOrEmpty())
                 welcomeWithNameTv.text = users.first().email
         })
-
-        buttonLogout.setOnClickListener {
-            logout.perform {
-                val intent = Intent(this@StartFragment.context, LoginActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                startActivity(intent)
-            }
-        }
     }
 }
